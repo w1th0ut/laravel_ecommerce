3fc3ea4d495c85c4ee602ab5fb277d74c791981a
Bagas Rizki <bagas.gunardi67@gmail.com>
2025-07-06 17:38:47 +0700

    Memperbaiki order dan product

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $casts = [
+        'billing_address' => 'array',
+        'shipping_address' => 'array',
+    ];
+    // Di dalam class Order
+    protected $fillable = [
+        'order_number',
+        'user_id',
+        'total_amount',
+        'status',
+        'billing_address',
+        'shipping_address',
+        'payment_method',
+        'payment_status'
+    ];
+    public function orderItems()
+    {
+        return $this->hasMany(\App\Models\OrderItem::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(\App\Models\User::class);
+    }
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity',
+        'price',
+        'total'
+    ];
+    public function product()
+    {
+        return $this->belongsTo(\App\Models\Product::class);
+    }
 }
